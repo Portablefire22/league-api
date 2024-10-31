3b7543012be92a5f930ebf0a14e82f953fa3bcaa
Portablefire22 <lilith@kitten.rs>
2024-10-31 00:26:20 +0000

    Added a function to convert a server region to a routing server

--- src/region/routing.rs
+++ src/region/routing.rs
@@ -1,7 +1,10 @@
+use super::server::ServerRegion;
+
 pub enum RoutingRegion {
     EUROPE,
     AMERICAS,
     ASIA,
+    SEA,
     ESPORTS,
 }
 
@@ -11,7 +14,30 @@ impl ToString for RoutingRegion {
             Self::EUROPE => String::from("europe"),
             Self::AMERICAS => String::from("americas"),
             Self::ASIA => String::from("asia"),
+            Self::SEA => String::from("sea"),
             Self::ESPORTS => String::from("esports"),
+        }
+    }
+}
+
+impl RoutingRegion {
+    pub fn from_server(server: ServerRegion) -> Self {
+        match &server {
+            ServerRegion::NA1 | ServerRegion::BR1 | ServerRegion::LA1 | ServerRegion::LA2 => {
+                Self::AMERICAS
+            }
+            ServerRegion::KR | ServerRegion::JP1 => Self::ASIA,
+            ServerRegion::EUN1
+            | ServerRegion::EUW1
+            | ServerRegion::ME1
+            | ServerRegion::TR1
+            | ServerRegion::RU => Self::EUROPE,
+            ServerRegion::OC1
+            | ServerRegion::PH2
+            | ServerRegion::SG2
+            | ServerRegion::TH2
+            | ServerRegion::TW2
+            | ServerRegion::VN2 => Self::SEA,
         }
     }
 }
